2b813d03bfe16475b4fb5d04be3a675783f8a5f2
hamdi <76883216+charradihamdi@users.noreply.github.com>
2022-04-22 04:23:35 +0100

    change post in back end application

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -2,16 +2,38 @@ import React, { useContext } from "react";
 import Log from "../components/Log";
 import { UidContext } from "../components/AppContext";
 import LeftNav from "../components/LeftNav";
-
+import Thread from '../components/Thread'
 const Profil = () => {
   const uid = useContext(UidContext);
-
+const clusters =()=>{
+  return (
+    <div className="main"> 
+    <div className="trending-container">
+      <h1>cluster1</h1>
+       <p>more to 35 milloins users</p>
+    </div>
+    <div className="trending-container">
+      <h1>cluster2</h1>
+       <p>more to 35 milloins users</p>
+    </div>
+    <div className="trending-container">
+      <h1>cluster3</h1>
+       <p>more to 35 milloins users</p>
+    </div>
+    <div className="trending-container">
+      <h1>cluster4</h1>
+       <p>more to 35 milloins users</p>
+    </div>
+    </div>
+    
+    )
+}
   return (
     <div className="home">
     <LeftNav />
     <div className="main">
       <div className="home-header">
-      {uid ?  <h1>clusters</h1>: <Log signin={true} signup={false} />}
+      {uid ? <div>{clusters()}</div>: <Log signin={true} signup={false} />}
       </div>
       
     </div>
