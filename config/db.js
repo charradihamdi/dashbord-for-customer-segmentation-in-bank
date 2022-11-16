9ed42b149db409e8d30eec3e837b83c4af386432
hamdi <charradihamdi1@gmail.com>
2022-11-17 00:13:22 +0100

    fc

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb://localhost/ecommerce",
-    {
+    "mongodb://localhost/ecommerce",  {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
